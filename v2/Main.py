e0fe44920a576489a12724a40dddb42eece54b60
Hamza Pavlovic <cow@cow.li>
2017-01-19 11:44:14 +0100

    First-class-citizen

--- v2/Main.py
+++ v2/Main.py
@@ -22,6 +22,10 @@ class Main():
                 return True
             return False
 
+    def quit_game(self):
+        pygame.quit()
+        sys.exit()
+
     def text_objects(self, text, font, color = WHITE):
         text_surface = font.render(text, True, color)
         return text_surface, text_surface.get_rect()
@@ -32,10 +36,14 @@ class Main():
         text_rectangle.left, text_rectangle.top = pos
         display.blit(text_surface, text_rectangle)
 
-    def create_button(self, display, content, x, y, w, h, ic, ac):
+    def create_button(self, display, content, x, y, w, h, ic, ac, func=False):
         mouse = pygame.mouse.get_pos()
         if x + w > mouse[0] > x and y + h > mouse[1] > y:
             pygame.draw.rect(display, ac, [x, y, w, h])
+            if func and pygame.mouse.get_pressed()[0] >= 1:
+                if func == self.main:
+                    self.init = False
+                func()
         else:
             pygame.draw.rect(display, ic, [x, y, w, h])
         button_font = SMALL_FONT
@@ -52,16 +60,15 @@ class Main():
             display.blit(BackGround.image, BackGround.rect)
             self.display_text(display, MED_FONT, "Euromast", BLACK, [self.window[0] * 0.415 - 35,20])
             ic, ac = BLACK, DARKBLACK
-            self.create_button(display, "Start", self.window[0]/2 - 75, self.window[1] /2.5, 150, 50, ic, ac)
+            self.create_button(display, "Start", self.window[0]/2 - 75, self.window[1] /2.5, 150, 50, ic, ac, self.main)
             self.create_button(display, "Ranglijst", self.window[0]/2 - 75, self.window[1] /2.5 + 60, 150, 50, ic, ac)
             self.create_button(display, "Instructies", self.window[0]/2 - 75, self.window[1] /2.5 + 120, 150, 50, ic, ac)
-            self.create_button(display, "Afsluiten", self.window[0]/2 - 75, self.window[1] /2.5 + 180, 150, 50, ic, ac)
+            self.create_button(display, "Afsluiten", self.window[0]/2 - 75, self.window[1] /2.5 + 180, 150, 50, ic, ac, self.quit_game)
 
             pygame.display.update()
             self.clock.tick(15)
         else:
-            pygame.quit()
-            sys.exit()
+            self.quit_game()
 
 
     def main(self):
@@ -72,12 +79,11 @@ class Main():
                 self.main_menu(DISPLAYSURFACE)
             else:
                 DISPLAYSURFACE.fill(WHITE)
-                self.display_text(DISPLAYSURFACE, "{Main game}", BLACK, [self.window[0] * 0.33,20])
+                self.display_text(DISPLAYSURFACE, SMALL_FONT, "{Main game}", BLACK, [self.window[0] * 0.33,20])
                 pygame.display.update()
-                clock.tick(15)
+                self.clock.tick(15)
         else:
-            pygame.quit()
-            sys.exit()
+            self.quit_game()
 
 
 Euromast = Main((595, 800), "Euromast")
