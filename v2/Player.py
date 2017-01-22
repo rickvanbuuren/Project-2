ac8cc23cb44a439884136c901ee10482a246ab9a
Hamza Pavlovic <cow@cow.li>
2017-01-22 17:39:44 +0100

    Added Player properties

--- v2/Player.py
+++ v2/Player.py
@@ -3,4 +3,8 @@ import pygame
 pygame.init()
 
 class Player(pygame.sprite.Sprite):
-    pass
+    def __init__(self, width, height, color):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((width, height))
+        self.surf.fill((color))
+        self.rect = self.surf.get_rect()
